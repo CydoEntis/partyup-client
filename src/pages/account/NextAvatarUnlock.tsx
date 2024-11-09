45af75242b2b56d2c5ec4d86a8b45657e93c0582
CydoEntis <cydoentis@gmail.com>
2024-11-09 00:07:44 -0500

    Added unlock tier to the next unlock preview.

--- src/pages/account/NextAvatarUnlock.tsx
+++ src/pages/account/NextAvatarUnlock.tsx
@@ -1,8 +1,8 @@
-import { Overlay, Center, SimpleGrid, Box,  } from "@mantine/core";
+import { Overlay, Center, SimpleGrid, Box, Divider } from "@mantine/core";
 import { useEffect } from "react";
 import UnlockableAvatar from "../../components/avatar/UnlockableAvatar";
 import useAvatarStore from "../../stores/useAvatarStore";
-import { Lock,  } from "lucide-react";
+import { Lock } from "lucide-react";
 import useAuthStore from "../../stores/useAuthStore";
 
 type NextUnlockProps = {};
@@ -23,45 +23,49 @@ function NextUnlock({}: NextUnlockProps) {
 	}, [getNextUnlockableTier]);
 
 	return (
-
-			<Box
-				pos="relative"
-				p={20}
-			>
-				{nextTier ? (
-					<p>Loading...</p>
-				) : (
-					<>
-						<Overlay
-							color="gray"
-							opacity={0.25}
-							radius="md"
+		<Box
+			pos="relative"
+			p={20}
+		>
+			<Divider
+				my="xs"
+				label={`Unlocked at tier ${nextUnlockableTierOfAvatars![0].tier}`}
+				labelPosition="center"
+			/>
+			{nextTier ? (
+				<p>Loading...</p>
+			) : (
+				<>
+					<Overlay
+						color="gray"
+						opacity={0.25}
+						radius="md"
+					/>
+					<Center
+						pos="absolute"
+						top="50%"
+						left="50%"
+						style={{ transform: "translate(-50%, -50%)" }}
+					>
+						<Lock
+							size={48}
+							color="white"
 						/>
-						<Center
-							pos="absolute"
-							top="50%"
-							left="50%"
-							style={{ transform: "translate(-50%, -50%)" }}
-						>
-							<Lock
-								size={48}
-								color="white"
-							/>
-						</Center>
-						<SimpleGrid cols={12}>
-							{nextUnlockableTierOfAvatars &&
-								user &&
-								nextUnlockableTierOfAvatars.map((avatar) => (
-									<UnlockableAvatar
-										key={avatar.id}
-										avatar={avatar}
-										user={user}
-									/>
-								))}
-						</SimpleGrid>
-					</>
-				)}
-			</Box>
+					</Center>
+					<SimpleGrid cols={12}>
+						{nextUnlockableTierOfAvatars &&
+							user &&
+							nextUnlockableTierOfAvatars.map((avatar) => (
+								<UnlockableAvatar
+									key={avatar.id}
+									avatar={avatar}
+									user={user}
+								/>
+							))}
+					</SimpleGrid>
+				</>
+			)}
+		</Box>
 	);
 }
 
