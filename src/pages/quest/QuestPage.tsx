934e4c3696bad1f0e61b2af1b476d5b89bbb8965
CydoEntis <cydoentis@gmail.com>
2024-11-14 18:39:46 -0500

    Began structuring layout of quest page

--- src/pages/quest/QuestPage.tsx
+++ src/pages/quest/QuestPage.tsx
@@ -1,6 +1,17 @@
 import { useEffect } from "react";
 import useQuestStore from "../../stores/useQuestStore";
 import { useParams } from "react-router-dom";
+import {
+	Box,
+	Paper,
+	Title,
+	Text,
+	SimpleGrid,
+	Avatar,
+	Stack,
+	Group,
+} from "@mantine/core";
+import UserAvatar from "../../components/avatar/UserAvatar";
 
 type Props = {};
 
@@ -21,9 +32,42 @@ function QuestPage({}: Props) {
 		fetchQuest();
 	}, [partyId, questId]);
 
+
+
 	if (!quest || loadingDetails) return <p>Loading...</p>;
 
-	return <div>{quest.title}</div>;
+	return (
+		<Box p={32}>
+			<SimpleGrid cols={2}>
+				<Paper
+					withBorder
+					p={16}
+				>
+					<Title>{quest.title}</Title>
+					<Text size="xl">{quest.description}</Text>
+				</Paper>
+				<Paper
+					withBorder
+					p={16}
+				>
+					<Stack gap={8}>
+						{quest.members.map((member) => (
+							<Group
+								align="center"
+								gap={8}
+							>
+								<UserAvatar
+									size="md"
+									avatar={member.avatar}
+								/>
+								<Text>{member.displayName}</Text>
+							</Group>
+						))}
+					</Stack>
+				</Paper>
+			</SimpleGrid>
+		</Box>
+	);
 }
 
 export default QuestPage;
