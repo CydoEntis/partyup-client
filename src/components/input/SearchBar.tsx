726799066c5883dde53351db1183ae1db920d639
CydoEntis <cydoentis@gmail.com>
2024-11-17 20:13:57 -0500

    Added search input clearing it's state when the clear all filters button is pressed.

--- src/components/input/SearchBar.tsx
+++ src/components/input/SearchBar.tsx
@@ -1,6 +1,8 @@
+import { useEffect } from "react";
 import { Button, Group, TextInput } from "@mantine/core";
 import { UseFormReturnType } from "@mantine/form";
 import { Search } from "lucide-react";
+import { useQueryParams } from "../../hooks/useQueryParams";
 
 type SearchBarProps = {
 	form: UseFormReturnType<{ search: string }>;
@@ -8,7 +10,15 @@ type SearchBarProps = {
 };
 
 function SearchBar({ form, onSearch }: SearchBarProps) {
+	const { getSearchParams } = useQueryParams();
 
+	useEffect(() => {
+		const params = getSearchParams();
+
+		if (!params.search) {
+			form.setFieldValue("search", "");
+		}
+	}, [getSearchParams().search]); 
 
 	const handleSearch = (values: { search: string }) => {
 		const searchTerm = values.search.trim();
